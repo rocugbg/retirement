4ff94718bb488325d968ca808ba883ee695cdd62
Bobby Cusicanqui <rocugbg@gmail.com>
2020-06-14 00:04:01 +0200

    Now show how much money you get every month until 85 years

--- retirement.py
+++ retirement.py
@@ -3,14 +3,19 @@ def formatCurrencyOutput(x):
 
 
 initialmoney = input("How much money do you start with? ")
-years_to_retire = int(input("How many years left before you need the money? "))
+years_of_age = int(input("How old are you? "))
+retire_age = int(input("At which age are you going to retire? "))
 adding_money_every_year = int(input("How much do you add every year? "))
 interest_rate = float(
     input("What is the percentage it will raise? (ex 1.13 = 13%) "))
 money_earned = float(initialmoney)
 
-for x in range(1, years_to_retire):
+for x in range(years_of_age, retire_age):
     money_earned = (money_earned + adding_money_every_year) * interest_rate
-    print(f"År {x} = {formatCurrencyOutput(money_earned)}")
+    print(f"År {x + 1} = {formatCurrencyOutput(money_earned)}")
 
-print(f"Till slut finns det {formatCurrencyOutput(money_earned)}")
+print()
+
+monthly_payment_from_savings = money_earned / (85 - retire_age) / 12
+
+print(f"You get {formatCurrencyOutput(monthly_payment_from_savings)} each month until you are 85 years.")
