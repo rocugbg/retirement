9f4041f8eb40e7fd8254614debdcd43bdf761ce7
Bobby Cusicanqui <rocugbg@gmail.com>
2020-06-15 22:13:29 +0200

    Update retirement.py

--- retirement.py
+++ retirement.py
@@ -2,20 +2,18 @@ def formatCurrencyOutput(x):
     return '{:,.0f}kr'.format(int(x))
 
 
-initialmoney = input("How much money do you start with? ")
-years_of_age = int(input("How old are you? "))
-retire_age = int(input("At which age are you going to retire? "))
-adding_money_every_year = int(input("How much do you add every year? "))
+initialmoney = input("Hur mycket pengar har du totalt sparat? ")
+years_of_age = int(input("Hur gammal är du? "))
+retire_age = int(input("Vid vilken ålder vill du sluta arbeta? "))
+monthly_payments_from_ppm_and_tjp = int(input("Hur mycket säger pension.se att du kommer få ut i månaden? "))
+adding_money_every_year = int(input("Hur mycket pengar sparar du varje år? "))
 interest_rate = float(
-    input("What is the percentage it will raise? (ex 1.13 = 13%) "))
+    input("Hur mycket tror du att ditt innehav kommer att öka per år? (ex 1.13 = 13%) "))
 money_earned = float(initialmoney)
-
+print()
 for x in range(years_of_age, retire_age):
     money_earned = (money_earned + adding_money_every_year) * interest_rate
     print(f"År {x + 1} = {formatCurrencyOutput(money_earned)}")
-
 print()
-
 monthly_payment_from_savings = money_earned / (85 - retire_age) / 12
-
-print(f"You get {formatCurrencyOutput(monthly_payment_from_savings)} each month until you are 85 years")
+print(f"Du kommer att få ut totalt {formatCurrencyOutput(monthly_payments_from_ppm_and_tjp + monthly_payment_from_savings)} varje månad tills du är 85 år.")
